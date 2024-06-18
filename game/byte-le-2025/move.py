e63f6efe69ab5e9835d4875a8065a44ae314a8e7
CaitlinAzazel <caitlin.frank03@gmail.com>
2024-06-18 11:05:01 -0500

    fixed some errors and placement
    changed location of subaction: Move | None to inside Move class, commented it out due to it not recognizing it as being defined. changed names of test move files and made sure they were actually testing, I believe they pass but it does not say passed, no longer produces errors.

--- game/byte-le-2025/move.py
+++ game/byte-le-2025/move.py
@@ -6,29 +6,25 @@ class Move:
     move_type: MoveType
     target_type: TargetType
     cost: int
-    # use_move(targets = None): -> None
-    # def use_move(targets = None)  -> None:
-
+    #subaction: Move | None
+    def use_move(targets = None)  -> None:
+        pass
 
 class Attack(Move):
     damage_points: int
     move_type: MoveType = MoveType.ATTACK
-    subaction: Move | None
 
 
 class Heal(Move):
     heal_points: float
     move_type: MoveType = MoveType.HEAL
-    subaction: Move | None
 
 
 class Buff(Move):
     modifier: float
     move_type: MoveType = MoveType.BUFF
-    subaction: Move | None
 
 
 class Debuff(Move):
     modifier: float
     move_type: MoveType = MoveType.DEBUFF
-    subaction: Move | None
--- game/byte-le-2025/test_move.py
+++ game/byte-le-2025/test_move.py
@@ -7,25 +7,33 @@ import game.test_suite.utils
 
 
 class TestMove(unittest.TestCase):
+    """
+        `Test Move Notes:`
 
+            This class tests the different methods in the Move class.
+        """
 
     def setUp(self) -> None:
         self.move: Move = Move()
+        self.ATTACK: Attack = ATTACK(move)
+        self.HEAL: Heal = HEAL(move)
+        self.DEBUFF: Debuff = DEBUFF(move)
+        self.BUFF: Buff = BUFF(move)
         self.utils = game.test_suite.utils
 
-    def testHeal(self)-> None:
-        heal_points = 5.5
+    def test_heal(self)-> None:
+        self.Heal = 5.5
         self.assertEqual(self.move.HEAL, 5.5)
 
-    def testAttack(self)-> None:
-        attack = 5
+    def test_attack(self)-> None:
+        self.Attack = 5
         self.assertEqual(self.move.ATTACK, 5)
 
 
-    def testBuff(self) -> None:
-        buff = 5.5
+    def test_buff(self) -> None:
+        self.Buff = 5.5
         self.assertEqual(self.move.move_type.BUFF(buff))
 
-    def testDebuff(self) -> None:
-        debuff = 5.5
+    def test_debuff(self) -> None:
+        self.Debuff = 5.5
         self.assertEqual(self.move.DEBUFF, 5.5)
