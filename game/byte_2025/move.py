b7c562f48040856831c7a4fe7f857e1dd6093ffd
CaitlinAzazel <caitlin.frank03@gmail.com>
2024-06-18 22:34:20 -0500

    Fixed move and testmove
    added getters, setters, json and proper tests

--- game/byte_2025/move.py
+++ game/byte_2025/move.py
@@ -5,17 +5,15 @@ from typing import Self
 
 class Move(GameObject):
 
-    def __init__(self, name: str = "", move_type: MoveType = MoveType.ATTACK,
-                 target_type: TargetType = TargetType.SINGLE_OPP, cost: int = 0,
+    def __init__(self, name: str = '', target_type: TargetType = TargetType.SINGLE_OPP, cost: int = 0, 
                  subaction: Self | None = None):
         super().__init__()
-        self.name: str
-        self.move_type: MoveType
-        self.target_type: TargetType
-        self.cost: int
-        self.subaction: Self | None
+        self.name: str = name
+        self.move_type = MoveType.MOVE
+        self.target_type: TargetType = target_type
+        self.cost: int = cost
+        self.subaction: Self | None = subaction
 
-    # GETTERS AND SETTERS
     @property
     def name(self) -> str:
         return self.__name
@@ -23,51 +21,197 @@ class Move(GameObject):
     @name.setter
     def name(self, name: str) -> None:
         if name is None or not isinstance(name, str):
-            raise ValueError(f'{self.__class__.__name__}.name must be a str. Name is currently a {name.__class__.__name__}.')
+            raise ValueError(f'{self.__class__.__name__}.name must be a str. Name is currently'
+                             f' a {name.__class__.__name__}.')
         self.__name: str = name
+
+    @property
+    def target_type(self) -> TargetType:
+        return self.__target_type
+
+    @target_type.setter
+    def target_type(self, target_type: TargetType) -> None:
+        if target_type is None or not isinstance(target_type, TargetType):
+            raise ValueError(f'{self.__class__.__name__}.target_type bust be a Target_type. Target_type is currentlt '
+                             f'a{target_type.__class__.__name__}.')
+        self.__target_type: TargetType = target_type
+
+    @property
+    def cost(self) -> int:
+        return self.__cost
+
+    @cost.setter
+    def cost(self, cost: int) -> None:
+        if cost is None or not isinstance(cost, int):
+            raise ValueError(f'{self.__class__.__name__}.cost must be a int. Cost is currently'
+                             f' a {cost.__class__.__name__}.')
+        self.__cost: int = cost
+
+    @property
+    def subaction(self):
+        return self.__subaction
+
+    @subaction.setter
+    def subaction(self, subaction: Self | None) -> None:
+        if subaction is not None and not isinstance(subaction, Move):
+            raise ValueError(f'{self.__class__.__name__}.subaction must be a None. Subaction is currently'
+                             f' a {subaction.__class__.__name__}.')
+        self.__subaction: Self = subaction
 
     def use_move(self, targets=None) -> None:
         pass
 
-    # TO AND FROM JSON
+    def to_json(self) -> dict:
+        data: dict = super().to_json()
+        data['name'] = self.name
+        data['move_type'] = self.move_type
+        data['target_type'] = self.target_type
+        data['cost'] = self.cost
+        data['subaction'] = self.subaction.to_json() if self.subaction is not None else None
+
+        return data
+
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.name: str = data['name']
+        self.move_type: MoveType = data['move_type']
+        self.target_type: TargetType = data['target_type']
+        self.cost: int = data['cost']
+        self.subaction: Self | None = data['subaction']
+
+        return self
 
 
 class Attack(Move):
-    def __init__(self, name: str = "", move_type: MoveType = MoveType.ATTACK,
-                 target_type: TargetType = TargetType.SINGLE_OPP, cost: int = 0,
-                 subaction: Self | None = None, damage_points: int = 0):
-        super().__init__(name, move_type,
-                 target_type, cost,
-                 subaction)
+    def __init__(self, name: str = '', target_type: TargetType = TargetType.SINGLE_OPP, cost: int = 0,
+                 subaction: Move | None = None, damage_points: int = 0):
+        super().__init__(name, target_type, cost, subaction)
+
         self.damage_points: int = damage_points
+        self.move_type: MoveType = MoveType.ATTACK
+
+    @property
+    def damage_points(self):
+        return self.__damage_points
+
+    @damage_points.setter
+    def damage_points(self, damage_points: int):
+        if damage_points is None or not isinstance(damage_points, int):
+            raise ValueError(f'{self.__class__.__name__}.damage_points must be an int. Damage_points is currently'
+                             f' a {damage_points.__class__.__name__}.')
+        self.__damage_points: int = damage_points
 
     def use_move(self, targets=None) -> None:
         # IMPLEMENT HERE
         pass
+
+    def to_json(self) -> dict:
+        data: dict = super().to_json()
+        data['damage_points'] = self.damage_points
+        return data
+
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.damage_points: int = data['damage_points']
+        return self
 
 
 class Heal(Move):
-    heal_points: float
-    move_type: MoveType = MoveType.HEAL
+    def __init__(self, name: str = '', target_type: TargetType = TargetType.SINGLE_ALLY, cost: int = 0,
+                 subaction: Move | None = None, heal_points: float = 0.0):
+        super().__init__(name, target_type, cost, subaction)
+
+        self.heal_points: float = heal_points
+        self.move_type: MoveType = MoveType.HEAL
+
+    @property
+    def heal_points(self):
+        return self.__heal_points
+
+    @heal_points.setter
+    def heal_points(self, heal_points: float):
+        if heal_points is None or not isinstance(heal_points, float):
+            raise ValueError(f'{self.__class__.__name__}.heal_points must be an float. Heal_points is currently'
+                             f' a {heal_points.__class__.__name__}.')
+        self.__heal_points: float = heal_points
 
     def use_move(self, targets=None) -> None:
-        # IMPLEMENT HERE
         pass
+
+    def to_json(self) -> dict:
+        data: dict = super().to_json()
+        data['heal_points'] = self.heal_points
+        return data
+
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.heal_points: int = data['heal_points']
+        return self
 
 
 class Buff(Move):
-    modifier: float
-    move_type: MoveType = MoveType.BUFF
+    def __init__(self, name: str = '', target_type: TargetType = TargetType.SINGLE_ALLY, cost: int = 0, 
+                 subaction: Move | None = None, buff_amount: float = 0.0):
+        super().__init__(name, target_type, cost, subaction)
+
+        self.buff_amount: float = buff_amount
+        self.move_type: MoveType = MoveType.BUFF
+
+    @property
+    def buff_amount(self):
+        return self.__buff_amount
+
+    @buff_amount.setter
+    def buff_amount(self, buff_amount: float):
+        if buff_amount is None or not isinstance(buff_amount, float):
+            raise ValueError(f'{self.__class__.__name__}.buff_amount must be an float. Buff_amount is currently'
+                             f' a {buff_amount.__class__.__name__}.')
+        self.__buff_amount: float = buff_amount
 
     def use_move(self, targets=None) -> None:
         # IMPLEMENT HERE
         pass
+
+    def to_json(self) -> dict:
+        data: dict = super().to_json()
+        data['buff_amount'] = self.buff_amount
+        return data
+
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.buff_amount: int = data['buff_amount']
+        return self
 
 
 class Debuff(Move):
-    modifier: float
-    move_type: MoveType = MoveType.DEBUFF
+    def __init__(self, name: str = '', target_type: TargetType = TargetType.SINGLE_OPP, cost: int = 0,
+                 subaction: Move | None = None, debuff_amount: float = 0.0):
+        super().__init__(name, target_type, cost, subaction)
+
+        self.debuff_amount: float = debuff_amount
+        self.move_type: MoveType = MoveType.DEBUFF
+
+    @property
+    def debuff_amount(self):
+        return self.__debuff_amount
+
+    @debuff_amount.setter
+    def debuff_amount(self, debuff_amount: float):
+        if debuff_amount is None or not isinstance(debuff_amount, float):
+            raise ValueError(f'{self.__class__.__name__}.debuff_amount must be an float. Debuff_amount is currently'
+                             f' a {debuff_amount.__class__.__name__}.')
+        self.__debuff_amount: float = debuff_amount
 
     def use_move(self, targets=None) -> None:
         # IMPLEMENT HERE
         pass
+
+    def to_json(self) -> dict:
+        data: dict = super().to_json()
+        data['debuff_amount'] = self.debuff_amount
+        return data
+
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.debuff_amount: int = data['debuff_amount']
+        return self
--- game/test_suite/tests/test_move.py
+++ game/test_suite/tests/test_move.py
@@ -1,38 +1,51 @@
 import unittest
 from game.common.enums import *
 from game.byte_2025.move import *
-import game.test_suite.utils
-
+from game.test_suite.utils import spell_check
 
 
 class TestMove(unittest.TestCase):
     """
-        `Test Move Notes:`
+    `Test Move Notes:`
 
-            This class tests the different methods in the Move class.
-        """
+        This class tests the different methods in the Move class.
+    """
 
     def setUp(self) -> None:
         self.move: Move = Move()
-        self.attack: Attack = Attack(name="Basic", damage_points=1)
-        self.HEAL: Heal = HEAL(move)
-        self.DEBUFF: Debuff = DEBUFF(move)
-        self.BUFF: Buff = BUFF(move)
-        self.utils = game.test_suite.utils
+        self.attack: Attack = Attack(name="Basic Attack", damage_points=1)
+        self.heal: Heal = Heal(name="Basic Heal", heal_points=1)
+        self.buff: Buff = Buff('Big Buff', target_type=TargetType.ALL_ALLY, buff_amount=2, subaction=self.attack)
+        self.debuff: Debuff = Debuff(name="Big Debuff", cost=2, debuff_amount=2)
 
-    def test_heal(self)-> None:
-        self.Heal = 5.5
-        self.assertEqual(self.move.HEAL, 5.5)
+    def test_attack_init(self) -> None:
+        self.assertEqual(self.attack.name, "Basic Attack")
+        self.assertEqual(self.attack.move_type, MoveType.ATTACK)
+        self.assertEqual(self.attack.target_type, TargetType.SINGLE_OPP)
+        self.assertEqual(self.attack.cost, 0)
+        self.assertEqual(self.attack.subaction, None)
+        self.assertEqual(self.attack.damage_points, 1)
 
-    def test_attack(self)-> None:
-        self.Attack = 5
-        self.assertEqual(self.move.ATTACK, 5)
+    def test_heal_init(self) -> None:
+        self.assertEqual(self.heal.name, "Basic Heal")
+        self.assertEqual(self.heal.move_type, MoveType.HEAL)
+        self.assertEqual(self.heal.target_type, TargetType.SINGLE_ALLY)
+        self.assertEqual(self.heal.cost, 0)
+        self.assertEqual(self.heal.subaction, None)
+        self.assertEqual(self.heal.heal_points, 1)
 
+    def test_buff_init(self) -> None:
+        self.assertEqual(self.buff.name, "Big Buff")
+        self.assertEqual(self.buff.move_type, MoveType.BUFF)
+        self.assertEqual(self.buff.target_type, TargetType.ALL_ALLY)
+        self.assertEqual(self.buff.cost, 0)
+        self.assertEqual(self.buff.subaction, self.attack)
+        self.assertEqual(self.buff.buff_amount, 2)
 
-    def test_buff(self) -> None:
-        self.Buff = 5.5
-        self.assertEqual(self.move.move_type.BUFF(buff))
-
-    def test_debuff(self) -> None:
-        self.Debuff = 5.5
-        self.assertEqual(self.move.DEBUFF, 5.5)
+    def test_debuff_init(self) -> None:
+        self.assertEqual(self.debuff.name, "Big Debuff")
+        self.assertEqual(self.debuff.move_type, MoveType.DEBUFF)
+        self.assertEqual(self.debuff.target_type, TargetType.SINGLE_OPP)
+        self.assertEqual(self.debuff.cost, 2)
+        self.assertEqual(self.debuff.subaction, None)
+        self.assertEqual(self.debuff.debuff_amount, 2)
