b3e25a836a7a3d6ba4e4fe9be05364a450ac826c
Gunnar Moody <moodygman04@gmail.com>
2024-06-26 11:01:43 -0600

    Update test_team_manager.py
    completed test team manager with tests to the json methods, speed sort method, and the filter by type method (all working)

--- game/test_suite/tests/test_team_manager.py
+++ game/test_suite/tests/test_team_manager.py
@@ -1,9 +1,9 @@
 import unittest
 
-from game.common.team_manager import TeamManager
 from game.byte_2025.character import *
-from game.common.enums import *
 from game.common.action import *
+from game.common.team_manager import TeamManager
+from game.byte_2025.move import *
 
 
 class TestTeamManager(unittest.TestCase):
@@ -11,9 +11,9 @@ class TestTeamManager(unittest.TestCase):
     Test for Team Manager class
     """
     def setUp(self):
-        self.character1: Character = Leader('Agles', CharacterType.TANK, 100, 10, 10, 10)
-        self.character2: Character = GenericAttacker('Grog',health=50, attack=15, defense=5, speed=5)
-        self.character3: Character = GenericHealer('Eden', health=60, attack=5, defense=15, speed=5)
+        self.character1: Character = Leader('Agles', CharacterType.TANK, 100, 10, 10, 5)
+        self.character2: Character = GenericAttacker('Grog', health=50, attack=15, defense=5, speed=15)
+        self.character3: Character = GenericHealer('Eden', health=60, attack=5, defense=15, speed=10)
         self.team_manager: TeamManager = TeamManager()
         self.team_manager2: TeamManager = TeamManager([self.character1, self.character2, self.character3])
 
@@ -50,4 +50,28 @@ class TestTeamManager(unittest.TestCase):
             self.team_manager.score = 'hi'
         self.assertEqual(str(e.exception), f'{self.team_manager.__class__.__name__}.score must be an int. '
                                            f'It is a(n) {str.__name__} and has the value of hi.')
-        
+
+    def test_speed_sort(self) -> None:
+        self.team_manager2.speed_sort()
+        self.assertEqual(self.team_manager2.team, [self.character2, self.character3, self.character1])
+
+    def test_filter_by_type(self) -> None:
+        self.assertEqual(self.team_manager.filter_by_type(CharacterType.ATTACKER), TeamManager().team)
+        self.assertEqual(self.team_manager.filter_by_type(CharacterType.TANK), [])
+        self.assertEqual(self.team_manager2.filter_by_type(CharacterType.TANK), [self.character1])
+        self.assertEqual(self.team_manager2.filter_by_type(CharacterType.ATTACKER), [self.character2])
+        self.assertEqual(self.team_manager2.filter_by_type(CharacterType.HEALER), [self.character3])
+
+    def test_json_default(self) -> None:
+        data: dict = self.team_manager.to_json()
+        team_manager: TeamManager = TeamManager().from_json(data)
+        self.assertEqual(team_manager.object_type, self.team_manager.object_type)
+        self.assertEqual(team_manager.team, self.team_manager.team)
+        self.assertEqual(team_manager.score, self.team_manager.score)
+
+    def test_json_unique(self) -> None:
+        data: dict = self.team_manager2.to_json()
+        team_manager2: TeamManager = TeamManager().from_json(data)
+        self.assertEqual(team_manager2.object_type, self.team_manager2.object_type)
+        self.assertEqual(team_manager2.team, self.team_manager2.team)
+        self.assertEqual(team_manager2.score, self.team_manager2.score)
