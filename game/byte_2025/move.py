91232b9977c18e00242ec0620a8b5d8cbf689b58
CaitlinAzazel <caitlin.frank03@gmail.com>
2024-06-25 20:46:57 -0500

    made suggested changes

--- game/byte_2025/move.py
+++ game/byte_2025/move.py
@@ -9,7 +9,7 @@ class Move(GameObject):
                  subaction: Self | None = None):
         super().__init__()
         self.name: str = name
-        self.move_type = MoveType.MOVE
+        self.move_type: MoveType
         self.target_type: TargetType = target_type
         self.cost: int = cost
         self.subaction: Self | None = subaction
