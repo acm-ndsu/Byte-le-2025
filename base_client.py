bb90f0fdeccf9bf5cd0d9ed1a38ad3a9c3dd6c60
Ian King <114031398+KingPhilip14@users.noreply.github.com>
2024-10-31 18:38:36 -0500

    Client (#24)
    * Updated the client files and the engine file to accept the team name and set the team
    
    * Updated the end condition of the game, and added logic to reward the winner at the end of a game
    
    * The differential points are given to both the winner and loser of the game

--- base_client.py
+++ base_client.py
@@ -1,6 +1,4 @@
 from game.client.user_client import UserClient
-from game.commander_clash.character.character import Generic, Leader
-from game.commander_clash.validate_team import validate_team_selection as validate
 from game.common.enums import *
 
 
@@ -9,27 +7,22 @@ class Client(UserClient):
     def __init__(self):
         super().__init__()
 
-    def team_name(self) -> str:
+    def team_data(self) -> tuple[str, tuple[SelectLeader, SelectGeneric, SelectGeneric]]:
         """
-        Allows the team to set a team name.
-        :return: Your team name
+        Returns your team name and a tuple of enums representing the characters you want for your team.
+        The tuple of the team must be ordered as (Leader, Generic, Generic). If an enum is not placed in the correct
+        order (e.g., (Generic, Leader, Leader)), whichever selection is incorrect will be swapped with a default value
+        of Generic Attacker.
         """
-        return 'Team 1'
-
-    def team_selection(self) -> tuple[Leader, Generic, Generic]:
-        """
-        Returns a tuple containing a Leader and 2 Generic characters in that order to build the user's team. If
-        a character is in the wrong position (i.e., Leader at index 1 or 2), it will be replaced by a default value of
-        Generic Attacker.
-        """
-        return validate(SelectLeader.FULTRA, SelectGeneric.GEN_ATTACKER, SelectGeneric.GEN_ATTACKER)
+        return 'Team 1', (SelectLeader.ANAHITA, SelectGeneric.GEN_ATTACKER, SelectGeneric.GEN_ATTACKER)
 
     # This is where your AI will decide what to do
-    def take_turn(self, turn, actions, world, avatar):
+    def take_turn(self, turn, actions, world, team_manager):
         """
         This is where your AI will decide what to do.
-        :param turn:        The current turn of the game.
-        :param actions:     This is the actions object that you will add effort allocations or decrees to.
-        :param world:       Generic world information
+        :param turn:         The current turn of the game.
+        :param actions:      This is the actions object that you will add effort allocations or decrees to.
+        :param world:        Generic world information
+        :param team_manager: A class that wraps the list of Characters to control
         """
         pass
