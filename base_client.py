978e991c631f44786df3a00dc6e7d9549a46bb78
KingPhilip14 <kingian404@gmail.com>
2025-01-10 21:22:30 -0600

    Refactored CharacterType to ClassType. Fixed Generic Trash bugs but need to fix the syncing issue

--- base_client.py
+++ base_client.py
@@ -24,7 +24,7 @@ class Client(UserClient):
         order (e.g., (Generic, Leader, Leader)), whichever selection is incorrect will be swapped with a default value
         of Generic Attacker.
         """
-        return 'Tiny Titans', (SelectGeneric.GEN_TANK, SelectLeader.CALMUS, SelectGeneric.GEN_TANK)
+        return 'Tiny Titans', (SelectGeneric.GEN_TANK, SelectLeader.NINLIL, SelectGeneric.GEN_TANK)
 
     def first_turn_init(self, team_manager: TeamManager):
         """
