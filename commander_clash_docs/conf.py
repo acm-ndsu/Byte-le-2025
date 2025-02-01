04f44639c22becfe313e834bdbf5940bc591f366
KingPhilip14 <kingian404@gmail.com>
2025-02-01 09:25:53 -0600

    Changed conf.py for docs

--- commander_clash_docs/conf.py
+++ commander_clash_docs/conf.py
@@ -13,7 +13,7 @@ author = 'NDSU ACM Byte-le 2025 Dev Team'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
