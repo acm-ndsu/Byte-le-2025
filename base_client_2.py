4d39ae1470ef95b246b8fb74243b4f5ebd5c78ef
Ian King <114031398+KingPhilip14@users.noreply.github.com>
2025-05-21 17:50:00 -0500

    Update base_client_2.py (#48)
    Added the 2nd place team's code from the 2025 competiton.

--- base_client_2.py
+++ base_client_2.py
@@ -1,4 +1,11 @@
+"""
+Wah
+Nick Althoff
+North Dakota State University
+"""
+
 import random
+from game.utils.vector import *
 
 from game.client.user_client import UserClient
 from game.commander_clash.character.character import Character
@@ -19,12 +26,12 @@ class Client(UserClient):
 
     def team_data(self) -> tuple[str, tuple[SelectGeneric, SelectLeader, SelectGeneric]]:
         """
-        Returns your team name and a tuple of enums representing the characters you want for your team.
-        The tuple of the team must be ordered as (Generic, Leader, Generic). If an enum is not placed in the correct
-        order (e.g., (Generic, Leader, Leader)), whichever selection is incorrect will be swapped with a default value
-        of Generic Attacker.
+        Returns your team name (to be shown on visualizer) and a tuple of enums representing the characters you
+        want for your team. The tuple of the team must be ordered as (Generic, Leader, Generic). If an enum is not
+        placed in the correct order (e.g., (Generic, Leader, Leader)), whichever selection is incorrect will be
+        swapped with a default value of Generic Attacker.
         """
-        return 'Altruists', (SelectGeneric.GEN_TANK, SelectLeader.ANAHITA, SelectGeneric.GEN_ATTACKER)
+        return 'Wah', (SelectGeneric.GEN_HEALER, SelectLeader.CALMUS, SelectGeneric.GEN_HEALER)
 
     def first_turn_init(self, team_manager: TeamManager):
         """
@@ -44,6 +51,8 @@ class Client(UserClient):
 
     # This is where your AI will decide what to do
     def take_turn(self, turn: int, actions: list[ActionType], world: GameBoard, team_manager: TeamManager):
+        game_map = world.game_map
+
         """
         This is where your AI will decide what to do.
         :param turn:         The current turn of the game.
@@ -62,18 +71,112 @@ class Client(UserClient):
             return []
 
         # determine if the active character is healthy
-        current_state: State = State.HEALTHY if self.get_health_percentage(active_character) >= 0.50 else State.UNHEALTHY
+        if self.get_health_percentage(active_character) >= 0.75:
+            current_state: State = State.HEALTHY
+        else:
+            current_state: State = State.UNHEALTHY
+
+        # set up my own variable so I know how it works better
+        enemy_leader = ""
+        my_side = active_character.position.as_tuple()[0]
+        if my_side == 1:
+            their_side = 0
+        else:
+            their_side = 1
+        game_world = [["", "", ""], ["", "", ""]]
+        leader_y_value = 7
+        for i in range(2):
+            for j in range(3):
+                if Vector(i, j) in game_map:
+                    game_world[i][j] = game_map[Vector(i, j)].get_top()
+                    if game_world[i][j].rank_type == RankType.LEADER and i == their_side:
+                        enemy_leader = game_world[i][j].object_type
+                    if game_world[my_side][j] != "":
+                        if game_world[my_side][j].rank_type == RankType.LEADER:
+                            leader_y_value = j
+        # Establish threats
+        priority = [0, 0, 0]
+        for j in range(3):
+            if game_world[their_side][j] != "":
+                if game_world[their_side][j].class_type == ClassType.ATTACKER:
+                    priority[j] += 2
+                if game_world[their_side][j].class_type == ClassType.HEALER:
+                    priority[j] += 5
+                    if enemy_leader == ObjectType.BERRY and game_world[their_side][j].rank_type == RankType.GENERIC:
+                        priority[j] += 5
+                if game_world[their_side][j].class_type == ClassType.TANK:
+                    priority[j] += 1
+                if game_world[their_side][j].rank_type == RankType.LEADER:
+                    priority[j] += 3
+                    if game_world[their_side][j].class_type == ClassType.HEALER:
+                        priority[i] -= 7
+                if game_world[their_side][j].rank_type == RankType.GENERIC:
+                    priority[j] += 1
+        # Find highest priority
+        highest_priority = 0
+        highest_priority_location = 0
+        for i in range(3):
+            if priority[i] > highest_priority:
+                highest_priority = priority[i]
+                highest_priority_location = i
 
         actions: list[ActionType]
+        leader_guy = ""
+        generics = []
+        for i in team_manager.team:
+            if i.rank_type == RankType.LEADER:
+                leader_guy = i
+            else:
+                generics.append(i)
+        total_health = 0
+        total_max_health = 0
+        if leader_guy != "":
+            total_health += leader_guy.current_health
+            total_max_health += leader_guy.max_health
+        if generics != []:
+            for i in generics:
+                total_health += i.current_health
+                total_max_health += i.max_health
 
-        if current_state == State.HEALTHY:
-            # if the active character from my team is healthy, use its Normal Move
-            actions = [ActionType.USE_NM]
-        else:
-            # if unhealthy, randomly decide to swap in a direction or use special 1
-            action: ActionType = random.choice([ActionType.SWAP_UP, ActionType.SWAP_DOWN, ActionType.USE_NM])
+        # Do the thing
+        if active_character.rank_type == RankType.GENERIC:
+            if leader_guy != "" and leader_y_value != highest_priority_location:
+                if leader_y_value > highest_priority_location and active_character.position.as_tuple()[
+                    1] == leader_y_value - 1:
+                    actions = [ActionType.SWAP_DOWN]
+                elif leader_y_value < highest_priority_location and active_character.position.as_tuple()[
+                    1] == leader_y_value + 1:
+                    actions = [ActionType.SWAP_UP]
+                else:
+                    if active_character.special_points >= 3 and total_health < total_max_health * 0.85:
+                        actions = [ActionType.USE_S2]
+                    elif active_character.current_health < 250:
+                        actions = [ActionType.USE_S1]
+                    else:
+                        actions = [ActionType.USE_NM]
+            else:
+                if active_character.special_points >= 3 and total_health < total_max_health * 0.85:
+                    actions = [ActionType.USE_S2]
+                elif active_character.current_health < 250 or game_world[their_side][
+                    active_character.position.as_tuple()[1]] == "":
+                    actions = [ActionType.USE_S1]
+                else:
+                    actions = [ActionType.USE_NM]
 
-            actions = [action]
+        if active_character.rank_type == RankType.LEADER:
+            if (generics == [] and leader_y_value != highest_priority_location) or (
+                    game_world[their_side][leader_y_value] == ""):
+                if leader_y_value > highest_priority_location:
+                    actions = [ActionType.SWAP_UP]
+                else:
+                    actions = [ActionType.SWAP_DOWN]
+            else:
+                if active_character.special_points >= 5 and active_character.current_health > 300 and active_character.attack.value < 99:
+                    actions = [ActionType.USE_S2]
+                #           elif active_character.special_points >= 3 and active_character.current_health > 200:
+                #              actions = [ActionType.USE_S1]
+                else:
+                    actions = [ActionType.USE_NM]
 
         return actions
 
@@ -84,6 +187,6 @@ class Client(UserClient):
         characters than the opponent.
         """
 
-        active_character: Character = team_manager.get_active_character(world.ordered_teams, world.active_pair_index)
+        active_character = team_manager.get_active_character(world.ordered_teams, world.active_pair_index)
 
         return active_character
